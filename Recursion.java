b9c6ee7f56b320d72ae7f2a392abc4783188929d
Taejoon Kim <tkim00@stuy.edu>
2019-01-31 21:09:55 -0500

    sqrt function finished

---
+++ Recursion.java
@@ -0,0 +1,19 @@
+public class Recursion {
+  public static void main(String[] args) {
+    System.out.println(sqrt(2,.00001));
+    System.out.println(sqrt(.2, .00001));
+  }
+
+  public static double sqrt(double n, double tolerance){
+    double guess = 1.0;
+    return sqrtH(n, tolerance, guess);
+  }
+  public static double sqrtH(double n, double tolerance, double guess){
+    if ((1+tolerance) * guess > ( n / guess + guess) / 2) ||
+    ((1-tolerance) * guess < ( n / guess + guess) / 2){
+      return sqrtH(n, tolerance, ( n / guess + guess) / 2)
+    } else {
+      return guess;
+    }
+  }
+}

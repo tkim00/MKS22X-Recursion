1f018a6eb30c37495e9f7f9e153aa98fb26e9984
Taejoon Kim <tkim00@stuy.edu>
2019-01-31 21:24:42 -0500

    sqrt tested and working

--- Recursion.java
+++ Recursion.java
@@ -1,7 +1,13 @@
 public class Recursion {
   public static void main(String[] args) {
+    //sqrt testing
     System.out.println(sqrt(2,.00001));
-    System.out.println(sqrt(.2, .00001));
+    System.out.println(sqrt(2,.01));
+    System.out.println(sqrt(5,.00001));
+    System.out.println(sqrt(100,.00001));
+    System.out.println(sqrt(.2,.00001));
+    System.out.println(sqrt(.25,.00001));
+
   }
 
   public static double sqrt(double n, double tolerance){
@@ -9,8 +15,8 @@ public class Recursion {
     return sqrtH(n, tolerance, guess);
   }
   public static double sqrtH(double n, double tolerance, double guess){
-    if (((1+tolerance) * guess > ( n / guess + guess) / 2) ||
-    ((1-tolerance) * guess < ( n / guess + guess) / 2)){
+    if (((1+tolerance) * guess < ( n / guess + guess) / 2) ||
+    ((1-tolerance) * guess > ( n / guess + guess) / 2)){
       return sqrtH(n, tolerance, ( n / guess + guess) / 2);
     } else {
       return guess;
